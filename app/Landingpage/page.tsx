33b688bf361c7f913c763a50893e14760394b767
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2025-12-11 18:34:35 +0100

    adding paragraphs to landing
    text section under hero

--- app/Landingpage/page.tsx
+++ app/Landingpage/page.tsx
@@ -1,11 +1,32 @@
 import Hero from "@/components/Hero/Hero";
 import Navbar from "@/components/Navbar/Navbar";
+import { Header, Paragraph } from "@/components/TextSection";
+import { heroTexts } from "@/lib/texts";
 
 const Landingpage = () => {
   return (
     <main className="relative flex flex-col h-screen items-center">
       <Navbar />
       <Hero />
+      <div className="bg-primary-background w-full h-[471px] flex flex-col">
+        <Header text={heroTexts.header} className="font-medium text-[34px]" />
+        <div className="flex  justify-center">
+          <div className="flex flex-col gap-6">
+            <Paragraph
+              paragraphs={heroTexts.paragraphs[0]}
+              className="font-light text-lg"
+            />
+            <Paragraph
+              paragraphs={heroTexts.paragraphs[1]}
+              className="font-light text-lg"
+            />
+          </div>
+          <Paragraph
+            paragraphs={heroTexts.paragraphs[2]}
+            className="font-light text-lg"
+          />
+        </div>
+      </div>
     </main>
   );
 };
--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -2,9 +2,9 @@ import CTA from "./CTA";
 
 const Hero = () => {
   return (
-    <section className="w-full h-full bg-[url(/heroImg.png)] bg-size-[150%_auto] bg-center">
+    <section className="w-full h-full bg-[url(/heroImg.png)] bg-cover bg-center bg-no-repeat">
       <div className="relative overflow-hidden w-full h-screen">
-        <div className="w-full h-screen flex justify-end items-center">
+        <div className="w-full h-full flex justify-end items-center">
           <CTA className="mr-8" />
         </div>
       </div>
