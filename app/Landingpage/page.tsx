3b5aec604957c09f980782f127a02a5be69d2a82
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2025-12-11 18:55:30 +0100

    adding textsection
    Adding the text and layout for the post hero section

--- app/Landingpage/page.tsx
+++ app/Landingpage/page.tsx
@@ -8,23 +8,31 @@ const Landingpage = () => {
     <main className="relative flex flex-col h-screen items-center">
       <Navbar />
       <Hero />
-      <div className="bg-primary-background w-full h-[471px] flex flex-col">
-        <Header text={heroTexts.header} className="font-medium text-[34px]" />
-        <div className="flex  justify-center">
-          <div className="flex flex-col gap-6">
-            <Paragraph
-              paragraphs={heroTexts.paragraphs[0]}
-              className="font-light text-lg"
-            />
-            <Paragraph
-              paragraphs={heroTexts.paragraphs[1]}
-              className="font-light text-lg"
-            />
-          </div>
-          <Paragraph
-            paragraphs={heroTexts.paragraphs[2]}
-            className="font-light text-lg"
+      <div className="bg-primary-background w-full  flex items-center justify-center ">
+        <div className="py-12">
+          <Header
+            text={heroTexts.header}
+            className="font-medium text-[34px] mb-4"
           />
+
+          <div className="flex  justify-center gap-8 ">
+            <div className="flex flex-col gap-6 max-w-[450px]">
+              <Paragraph
+                paragraphs={heroTexts.paragraphs[0]}
+                className="font-light text-lg"
+              />
+              <Paragraph
+                paragraphs={heroTexts.paragraphs[1]}
+                className="font-light text-lg"
+              />
+            </div>
+            <div className="flex flex-col gap-6 max-w-[450px]">
+              <Paragraph
+                paragraphs={heroTexts.paragraphs[2]}
+                className="font-light text-lg"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </main>
